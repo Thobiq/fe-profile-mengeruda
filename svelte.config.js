44f3ebc04800b91279049e2e0d6f77157fccdf61
Thobiq <malyasyithobiq@gmail.com>
2026-07-24 21:17:00 +0800

    fixing for update

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,10 @@ const config = {
       fallback: 'index.html',
       precompress: false,
       strict: false
-    })
+    }),
+    prerender: {
+      handleUnseenRoutes: 'ignore'
+    }
   }
 };
 
